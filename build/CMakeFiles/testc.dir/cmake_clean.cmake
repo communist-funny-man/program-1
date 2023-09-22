b8a8d3b1aa43ee0d027110f2ad85b543fab856ff
unknown <danidezaprieto@gmail.com>
2023-09-22 14:22:02 +0200

    CMake build tasks set

---
+++ build/CMakeFiles/testc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testc.dir/main.c.obj"
+  "CMakeFiles/testc.dir/main.c.obj.d"
+  "libtestc.dll.a"
+  "testc.exe"
+  "testc.exe.manifest"
+  "testc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Usuario/Desktop/Proyectos Pro1/practicas-pro1/program-1
+# Build directory: C:/Users/Usuario/Desktop/Proyectos Pro1/practicas-pro1/program-1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
