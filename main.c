cdf028ba4e0b5e162a3102aec525ca570869b628
communist-funny-man <danidezaprieto@gmail.com>
2023-09-26 16:29:37 +0200

    Corrected variable placement

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main(){
-    printf("hola");
     char name[50];
     char apellido1[20];
     char apellido2[20];
@@ -9,11 +8,6 @@ int main(){
     float credit2;
     float credit3;
 
-
-    float costem1 = credit1*15.45;
-    float costem2 = (credit2);
-    float costem3 = (credit2);
-
     printf("Introduzca su nombre utilizando hasta 50 carácteres\n");
     fgets(&name,sizeof name,stdin);
     printf("Introduzca sus apellidos utilizando hasta 20 carácteres por apellido\n");
@@ -21,4 +15,11 @@ int main(){
     printf("Introduzca su edad\n");
     scanf("%d",&age);
     printf("Introduzca sus créditos en la matrícula\n");
-    scanf("%f%f%f",&credit1,&credit2,&credit3);
+    scanf("%lf%lf%lf",&credit1,&credit2,&credit3);
+    
+    float costem1 = (credit1*15.45);
+    float costem2 = (credit2*(15.45*1.25));
+    float costem3 = (credit2*(15.45*1.5));
+    
+    printf("%lf\n%lf\n%lf",costem1,costem2,costem3);
+}
